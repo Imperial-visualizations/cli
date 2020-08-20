0e72891e524b6d43d435c0a8dff89c4001546990
Robert King <robertkng104@gmail.com>
2020-08-20 18:57:58 +0100

    added gitignores to all folders to ensure that node_modules is never commited

--- src/main.js
+++ src/main.js
@@ -90,7 +90,7 @@ Handlebars.registerHelper("if_eq",function(a,b,opts) {
     if( a == b){
         return opts.fn(this);
     }else{
-        return opts.inverse(this)
+        return opts.inverse(this);
     }
 });
 
