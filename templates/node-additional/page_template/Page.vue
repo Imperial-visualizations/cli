b425b6fbad574f01a89ec3508f28464a2c66c322
Robert King <robertkng104@gmail.com>
2020-09-24 15:38:26 +0100

    made sure page templates point to common assets folder

--- templates/node-additional/page_template/Page.vue
+++ templates/node-additional/page_template/Page.vue
@@ -2,7 +2,7 @@
     <div>
         <iv-visualisation :title="pageName">
             <div class="iv-welcome-message">
-                <img src='./assets/ImpVis-logo.png' alt="ImpVisLogo" height="50"/>
+                <img src='@/assets/ImpVis-logo.png' alt="ImpVisLogo" height="50"/>
                 <h1> Welcome to Imperial Visualisations!</h1>
                 <p> Your project, {{pageName}} has succesfully been set up using the multiPage CLI template!</p>
             </div>
