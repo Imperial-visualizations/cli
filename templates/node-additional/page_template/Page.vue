1d80dee1704e2010242c3d5a227bdc758fe67761
Robert King <robertkng104@gmail.com>
2020-09-24 15:03:45 +0100

    updated page templates to be more interesting, updated script tag template to actually work properly

--- templates/node-additional/page_template/Page.vue
+++ templates/node-additional/page_template/Page.vue
@@ -1,15 +1,29 @@
 <template>
-    <div id="{{pageName}}">
-        <iv-visualisation :title="name">
-            This is page {{pageName}}
+    <div>
+        <iv-visualisation :title="pageName">
+            <div class="iv-welcome-message">
+                <img src='./assets/ImpVis-logo.png' alt="ImpVisLogo" height="50"/>
+                <h1> Welcome to Imperial Visualisations!</h1>
+                <p> Your project, {{pageName}} has succesfully been set up using the multiPage CLI template!</p>
+            </div>
         </iv-visualisation>
     </div>
 </template>
 <script>
 export default {
-    name:'{{pageName}}'
+    name:"{{squash pageName}}",
+    data(){
+        return {
+            pageName:{{pageName}}
+        }
+    }
 }
 </script>
 <style>
-
+.iv-welcome-message{
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 50px;
+}
 </style>
--- templates/node/src/App.vue
+++ templates/node/src/App.vue
@@ -4,8 +4,7 @@
             <div class="iv-welcome-message">
                 <img src='./assets/ImpVis-logo.png' alt="ImpVisLogo" height="50"/>
                 <h1> Welcome to Imperial Visualisations!</h1>
-                <p> Your project, {{projectName}} has succesfully been set up!</p>
-                <p> There is a vue instance up and running!</p>
+                <p> Your project, {{projectName}} has succesfully been set up using the NodeJS template!</p>
             </div>
         </iv-visualisation>
     </div>
