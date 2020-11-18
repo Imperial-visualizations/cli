7967874c74df3564ea476dcb81c1bfe19ec76c17
Robert King <robertkng104@gmail.com>
2020-11-18 13:32:24 +0000

    fixes #2 by adding in a cross platform node script which executes impvis create in the approrpriate OS shell (CMD/bash)

---
+++ bin/create-impvis.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+var execa = require('execa')
+var chalk = require('chalk')
+console.log( chalk.yellow('WARNING') + ' create-impvis has been depreciated, please use \"impvis create\" going forward')
+try{
+    execa.sync('impvis',['create',...process.argv.slice(2)],{shell:true,stdio:'inherit'})
+}
+catch(err){
+    //This is left intentionally blank
+    //This is because impvis process (or rather the spawned shell) catches the keyboard interupt independently of us and then quits itself
+    //As this appears to execa, the process has quit unexpectdly and thus throws an error.
+    //There is probably a smarter way to handle this, but at the moment this solution is a lazy fix for those who still use create-impvis
+}
+
