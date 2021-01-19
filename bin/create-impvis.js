ff8ad3719f6545f4e2024f823b20cbed2e50cfb6
Robert King <robertkng104@gmail.com>
2021-01-19 12:27:57 +0000

    update to 1.0.10, make create-impvis guidance a bit more helpful

--- bin/create-impvis.js
+++ bin/create-impvis.js
@@ -2,7 +2,7 @@
 var execa = require('execa')
 var chalk = require('chalk')
 console.log( chalk.yellow('WARNING') + ' create-impvis has been depreciated, please use \"impvis create\" going forward')
-try{
+setTimeout( ()=>{try{
     execa.sync('impvis',['create',...process.argv.slice(2)],{shell:true,stdio:'inherit'})
 }
 catch(err){
@@ -11,4 +11,4 @@ catch(err){
     //As this appears to execa, the process has quit unexpectdly and thus throws an error.
     //There is probably a smarter way to handle this, but at the moment this solution is a lazy fix for those who still use create-impvis
 }
-
+},2000);
